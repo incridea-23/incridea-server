ffeb91a8ba753d8db23d6180635e73bf14de8113
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-02-12 11:27:56 +0530

    feat: RazorPay weebhook

--- src/models/PaymentOrder/mutation.ts
+++ src/models/PaymentOrder/mutation.ts
@@ -1,5 +1,7 @@
 import { builder } from "../../builder";
 import Razorpay from "razorpay";
+import { v4 as uuidv4 } from "uuid";
+
 enum OrderType {
   FEST_REGISTRATION = "FEST_REGISTRATION",
   EVENT_REGISTRATION = "EVENT_REGISTRATION",
@@ -14,18 +16,57 @@ builder.mutationField("createPaymentOrder", (t) =>
         required: true,
       }),
       eventId: t.arg({
-        type: "String",
+        type: "ID",
+        required: false,
       }),
     },
     resolve: async (query, root, args, ctx, info) => {
       const user = await ctx.user;
       if (!user) {
-        throw new Error("User not found");
+        throw new Error("Not authenticated");
       }
       const razorpay = new Razorpay({
         key_id: process.env.RAZORPAY_KEY,
         key_secret: process.env.RAZORPAY_SECRET,
       });
+      if (args.type === OrderType.EVENT_REGISTRATION) {
+        // EVENT_REGISTRATION
+        if (!args.eventId) {
+          throw new Error("Event Id not provided");
+        }
+        const event = await ctx.prisma.event.findUnique({
+          where: {
+            id: args.eventId as number,
+          },
+        });
+        if (!event) {
+          throw new Error("Event not found");
+        }
+        const payment_capture = 1;
+        const amount = event.fees;
+        const currency = "INR";
+        const options = {
+          amount: (amount * 100).toString(),
+          currency,
+          payment_capture,
+          recept: uuidv4(),
+        };
+        const response = await razorpay.orders.create(options);
+        return ctx.prisma.paymentOrder.create({
+          data: {
+            amount: response.amount,
+            status: "PENDING",
+            type: args.type,
+            User: {
+              connect: {
+                id: user.id,
+              },
+            },
+            orderId: response.id as string,
+          },
+        });
+      }
+      // FEST_REGISTRATION
       const payment_capture = 1;
       const amount = 250;
       const currency = "INR";
@@ -33,11 +74,12 @@ builder.mutationField("createPaymentOrder", (t) =>
         amount: (amount * 100).toString(),
         currency,
         payment_capture,
+        recept: uuidv4(),
       };
       const response = await razorpay.orders.create(options);
       return ctx.prisma.paymentOrder.create({
         data: {
-          amount: 250,
+          amount: response.amount,
           status: "PENDING",
           type: args.type,
           User: {
@@ -45,7 +87,7 @@ builder.mutationField("createPaymentOrder", (t) =>
               id: user.id,
             },
           },
-          orderId: response.id,
+          orderId: response.id as string,
         },
       });
     },
--- src/webhook/capture.ts
+++ src/webhook/capture.ts
@@ -1,7 +1,38 @@
 import { Request, Response } from "express";
+import { prisma } from "../utils/db/prisma";
 
-export function handler({ req, res }: { req: Request; res: Response }) {
-  const body = req.body;
-  console.log(body);
-  res.status(200).send("OK");
+export async function handler({ req, res }: { req: Request; res: Response }) {
+  if (req.method !== "POST") {
+    return res.status(405).send({ message: "Only POST requests allowed" });
+  }
+  try {
+    if (req.body.payload.payment.entity.status !== "captured") {
+      return res.status(400).send({ message: "Payment not captured" });
+    }
+    const order_id = req.body?.payload?.payment?.entity?.order_id as string;
+    const paymentOrder = await prisma.paymentOrder.update({
+      where: {
+        orderId: order_id,
+      },
+      data: {
+        status: "SUCCESS",
+        paymentData: JSON.stringify(req.body.payload.payment.entity),
+      },
+    });
+
+    if (paymentOrder.type === "FEST_REGISTRATION") {
+      await prisma.user.update({
+        where: {
+          id: paymentOrder.userId,
+        },
+        data: {
+          role: "PARTICIPANT",
+        },
+      });
+      return res.status(200).json({ status: "OK" });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(err);
+  }
 }
