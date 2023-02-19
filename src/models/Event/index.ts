ad51500167a3adf6bf8de2dcbfbd473fa0ff26b7
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-02-20 00:34:48 +0530

    fix: event type

--- src/models/Event/index.ts
+++ src/models/Event/index.ts
@@ -7,7 +7,18 @@ builder.prismaObject("Event", {
     id: t.exposeID("id"),
     name: t.exposeString("name"),
     fees: t.exposeInt("fees"),
-    description: t.exposeString("description"),
+    description: t.expose("description", {
+      type: "String",
+      nullable: true,
+    }),
+    venue: t.expose("venue", {
+      type: "String",
+      nullable: true,
+    }),
+    image: t.expose("image", {
+      type: "String",
+      nullable: true,
+    }),
     branch: t.relation("Branch"),
     organizer: t.relation("Organizer"),
     eventDate: t.expose("eventDate", {
