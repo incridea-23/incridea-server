f1da7fbac5df3504ca870c3a2ad2895d90cb9c5a
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-02-20 22:58:12 +0530

    feat: update/delete event

--- src/models/Event/mutation.ts
+++ src/models/Event/mutation.ts
@@ -1,8 +1,20 @@
+import { type } from "os";
 import { builder } from "../../builder";
 
 const EventCreateInput = builder.inputType("EventCreateInput", {
   fields: (t) => ({
     name: t.string({ required: true }),
+    description: t.string({ required: false }),
+    eventDate: t.field({
+      type: "Date",
+      required: false,
+    }),
+    venue: t.string({ required: false }),
+  }),
+});
+const EventUpdateInput = builder.inputType("EventUpdateInput", {
+  fields: (t) => ({
+    name: t.string({ required: false }),
     description: t.string({ required: false }),
     eventDate: t.field({
       type: "Date",
@@ -44,6 +56,126 @@ builder.mutationField("createEvent", (t) =>
           },
         },
       });
+    },
+  })
+);
+
+builder.mutationField("updateEvent", (t) =>
+  t.prismaField({
+    type: "Event",
+    args: {
+      id: t.arg({ type: "Int", required: true }),
+      data: t.arg({
+        type: EventUpdateInput,
+        required: true,
+      }),
+    },
+    errors: {
+      types: [Error],
+    },
+    resolve: async (query, root, args, ctx, info) => {
+      const user = await ctx.user;
+      if (!user) throw new Error("Not authenticated");
+      if (user.role !== "BRANCH_REP" && user.role !== "ORGANIZER")
+        throw new Error("No Permission");
+      const event = await ctx.prisma.event.findUnique({
+        where: {
+          id: args.id,
+        },
+      });
+      if (!event) throw new Error(`No Event with id ${args.id}`);
+      if (user.role === "BRANCH_REP") {
+        const branchRep = await ctx.prisma.branchRep.findUnique({
+          where: {
+            userId: user.id,
+          },
+        });
+        if (!branchRep) throw new Error(`No Branch Under ${user.name}`);
+
+        if (event.branchId !== branchRep.branchId)
+          throw new Error(`You are not authorized to update this event`);
+      }
+      if (user.role === "ORGANIZER") {
+        const organizer = await ctx.prisma.organizer.findUnique({
+          where: {
+            userId_eventId: {
+              userId: user.id,
+              eventId: args.id,
+            },
+          },
+        });
+        if (!organizer)
+          throw new Error(
+            `Oops ${user.name}! you are not an organizer of this event`
+          );
+      }
+
+      return ctx.prisma.event.update({
+        where: {
+          id: args.id,
+        },
+
+        data: {
+          ...args.data,
+          name: args.data.name || event.name,
+        },
+        ...query,
+      });
+    },
+  })
+);
+
+builder.mutationField("deleteEvent", (t) =>
+  t.field({
+    type: "String",
+    args: {
+      id: t.arg({ type: "Int", required: true }),
+    },
+    errors: {
+      types: [Error],
+    },
+    resolve: async (root, args, ctx) => {
+      const user = await ctx.user;
+      if (!user) throw new Error("Not authenticated");
+      if (user.role !== "BRANCH_REP" && user.role !== "ORGANIZER")
+        throw new Error("No Permission");
+      const event = await ctx.prisma.event.findUnique({
+        where: {
+          id: args.id,
+        },
+      });
+      if (!event) throw new Error(`No Event with id ${args.id}`);
+      if (user.role === "BRANCH_REP") {
+        const branchRep = await ctx.prisma.branchRep.findUnique({
+          where: {
+            userId: user.id,
+          },
+        });
+        if (!branchRep) throw new Error(`No Branch Under ${user.name}`);
+
+        if (event.branchId !== branchRep.branchId)
+          throw new Error(`You are not authorized to delete this event`);
+      }
+      if (user.role === "ORGANIZER") {
+        const organizer = await ctx.prisma.organizer.findUnique({
+          where: {
+            userId_eventId: {
+              userId: user.id,
+              eventId: args.id,
+            },
+          },
+        });
+        if (!organizer)
+          throw new Error(
+            `Oops ${user.name}! you are not an organizer of this event`
+          );
+      }
+      await ctx.prisma.event.delete({
+        where: {
+          id: args.id,
+        },
+      });
+      return "Event Deleted Successfully";
     },
   })
 );
