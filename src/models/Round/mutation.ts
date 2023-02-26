3696a5d51bf6e5f2883c294be2e0cbce0f05b5a4
AaronNazareth <4nm20cs005@nmamit.in>
2023-02-26 14:32:14 +0530

    feat: roundCompleted mutation

--- src/models/Round/mutation.ts
+++ src/models/Round/mutation.ts
@@ -108,3 +108,62 @@ builder.mutationField("deleteRound", (t) =>
     },
   })
 );
+
+builder.mutationField("roundCompleted", (t) =>
+    t.prismaField({
+      type: "Round",
+      args: {
+        eventId: t.arg.id({ required: true }),
+        roundNo: t.arg.int({ required: true }),
+      },
+      errors: {
+        types: [Error],
+      },
+      resolve: async (query, root, args, ctx, info) => {
+        const user = await ctx.user;
+        if (!user) 
+          throw new Error("Not authenticated");
+        
+        if (user.role !== "ORGANIZER") 
+          throw new Error("Not authorized");
+        
+        const event = await ctx.prisma.event.findUnique({
+          where: {
+            id: Number(args.eventId),
+          },
+          include: {
+            Organizers: true,
+          },
+        });
+
+        if (!event)
+          throw new Error("Event not found");
+
+        if (!event.Organizers.find((o) => o.userId === user.id))
+          throw new Error("Not authorized");
+
+        const round = await ctx.prisma.round.findUnique({
+          where: {
+            eventId_roundNo: {
+              eventId: Number(args.eventId),
+              roundNo: args.roundNo,
+            },
+          }
+        });
+        if (!round)
+          throw new Error("Round not found");
+
+        return ctx.prisma.round.update({
+          where: {
+            eventId_roundNo: {
+              eventId: Number(args.eventId),
+              roundNo: args.roundNo,
+            },
+          },
+          data: {
+            completed: true,
+          },
+        });
+      }
+    })
+);
