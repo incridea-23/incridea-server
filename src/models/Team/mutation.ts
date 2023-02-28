1359accd44ee4810f4c2673d070607211e45fd6a
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-02-28 22:25:36 +0530

    feat: mutation OrganizerRegisterSolo

--- src/models/Team/mutation.ts
+++ src/models/Team/mutation.ts
@@ -678,3 +678,92 @@ builder.mutationField("organizerMarkAttendance", (t) =>
     },
   })
 );
+
+builder.mutationField("OrganizerRegisterSolo", (t) =>
+  t.prismaField({
+    type: "Team",
+    args: {
+      eventId: t.arg.id({ required: true }),
+      userId: t.arg.id({ required: true }),
+    },
+    errors: {
+      types: [Error],
+    },
+    resolve: async (query, root, args, ctx, info) => {
+      const user = await ctx.user;
+      if (!user) {
+        throw new Error("Not authenticated");
+      }
+      if (user.role !== "ORGANIZER") {
+        throw new Error("Not authorized");
+      }
+      const event = await ctx.prisma.event.findUnique({
+        where: {
+          id: Number(args.eventId),
+        },
+        include: {
+          Organizers: true,
+        },
+      });
+      if (!event) {
+        throw new Error("Event not found");
+      }
+      if (
+        event.Organizers.filter((org) => org.userId === user.id).length === 0
+      ) {
+        throw new Error("Not authorized");
+      }
+
+      const participant = await ctx.prisma.user.findUnique({
+        where: {
+          id: Number(args.userId),
+        },
+      });
+      if (
+        !participant ||
+        participant.role == "USER" ||
+        participant.role === "JUDGE"
+      ) {
+        throw new Error(`No participant with id ${args.userId}`);
+      }
+      const registered = await ctx.prisma.team.findMany({
+        where: {
+          OR: [
+            {
+              eventId: event.id,
+            },
+            {
+              TeamMembers: {
+                some: {
+                  userId: Number(args.userId),
+                },
+              },
+            },
+          ],
+        },
+      });
+      if (
+        (event.eventType === "INDIVIDUAL" ||
+          event.eventType === "INDIVIDUAL_MULTIPLE_ENTRY") &&
+        registered.length > 0
+      )
+        throw new Error("");
+      const team = await ctx.prisma.team.create({
+        data: {
+          eventId: Number(args.eventId),
+          name: args.userId as string,
+          attended: true,
+          confirmed: true,
+          leaderId: Number(args.userId),
+        },
+      });
+      await ctx.prisma.teamMember.create({
+        data: {
+          userId: participant.id,
+          teamId: team.id,
+        },
+      });
+      return team;
+    },
+  })
+);
