ce7331136ded38604d4b2f790f98b1884656fc57
Prayag Sharma <85249351+prg2308@users.noreply.github.com>
2023-03-05 22:26:24 +0530

    feat:image-upload

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import { context } from "./context";
 import { schema } from "./schema";
 import bodyParser from "body-parser";
 import { handler as razorpayCapture } from "./webhook/capture";
+import { uploader as imageUpload } from "./cloudinary/upload";
+
+import { config } from "./cloudinary/config";
+const { upload } = config
+
 const port = Number(process.env.API_PORT) || 4000;
 const yoga = createYoga({
   context,
@@ -17,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/graphql", yoga);
 app.post("/webhook/capture", razorpayCapture);
+app.post("/cloudinary/upload", upload.single('image'), imageUpload);
 
 app.listen(port, () => {
   console.log(`🚀 Server ready at: http://localhost:4000/graphql`);
