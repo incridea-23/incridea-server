96002b9475c546fe2352f36d787e8c7cb6e4caac
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-04-05 19:56:00 +0530

    fix: EventPAymentOrder Object

--- src/models/PaymentOrder/index.ts
+++ src/models/PaymentOrder/index.ts
@@ -10,3 +10,13 @@ builder.prismaObject("PaymentOrder", {
     user: t.relation("User"),
   }),
 });
+
+builder.prismaObject("EventPaymentOrder", {
+  fields: (t) => ({
+    id: t.exposeID("id"),
+    amount: t.exposeInt("amount"),
+    status: t.exposeString("status"),
+    orderId: t.exposeID("orderId"),
+    Team: t.relation("Team"),
+  }),
+});
