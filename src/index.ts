e1b18bfc643ade81f61802ef61ee3d02a148f310
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-04-10 12:46:47 +0530

    fix: homepage route

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,14 @@ import { handler as razorpayCapture } from "./webhook/capture";
 import { uploader as imageUpload } from "./cloudinary/upload";
 
 import { config } from "./cloudinary/config";
-const { upload } = config
+const { upload } = config;
 
-import { useDepthLimit } from '@envelop/depth-limit'
+import { useDepthLimit } from "@envelop/depth-limit";
 const port = Number(process.env.API_PORT) || 4000;
 const yoga = createYoga({
   context,
   schema,
-  plugins: [useDepthLimit({ maxDepth: 6 })] //max depth allowed to avoid infinite nested queries
+  plugins: [useDepthLimit({ maxDepth: 6 })], //max depth allowed to avoid infinite nested queries
 });
 
 const app = express();
@@ -25,10 +25,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.get("/", (req, res) => {
+  res.send("Hello Incridea");
+});
 app.use("/graphql", yoga);
 app.post("/webhook/capture", razorpayCapture);
-app.post("/cloudinary/upload/:eventName", upload.single('image'), imageUpload);
+app.post("/cloudinary/upload/:eventName", upload.single("image"), imageUpload);
 
 app.listen(port, () => {
   console.log(`🚀 Server ready at: http://localhost:4000/graphql`);
