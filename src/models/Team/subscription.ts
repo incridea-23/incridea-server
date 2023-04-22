b4775e2c39736c36110838b39a8f077404320100
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-04-23 00:40:46 +0530

    feat: judgeGetTeamsByRound Subscription

--- src/models/Team/subscription.ts
+++ src/models/Team/subscription.ts
@@ -1,57 +1,47 @@
-import { resolve } from "path";
 import { builder } from "../../builder";
 
-builder.queryField("teams", (t) =>
+builder.queryField("judgeGetTeamsByRound", (t) =>
   t.prismaField({
     type: ["Team"],
-    smartSubscription: true,
-    subscribe: (subscription, root, args, ctx, info) => {
-      subscription.register("teams-added");
-    },
-    resolve: (query, root, args, ctx, info) => {
-      return ctx.prisma.team.findMany({
-        ...query,
-      });
-    },
-  })
-);
-
-builder.prismaObject("test", {
-  fields: (t) => ({
-    id: t.exposeID("id"),
-    name: t.exposeString("name"),
-  }),
-});
-builder.queryField("tests", (t) =>
-  t.prismaField({
-    type: ["test"],
-    smartSubscription: true,
-    subscribe: (subscription, root, args, ctx, info) => {
-      console.log(args, info, root);
-      subscription.register("test-event");
-    },
-    resolve: (query, root, args, ctx, info) => {
-      return ctx.prisma.test.findMany({
-        ...query,
-      });
-    },
-  })
-);
-
-builder.mutationField("addTest", (t) =>
-  t.prismaField({
-    type: "test",
     args: {
-      name: t.arg.string({ required: true }),
+      eventId: t.arg.int({ required: true }),
+      roundId: t.arg.int({ required: true }),
+    },
+    smartSubscription: true,
+    subscribe: (subscription, root, args, ctx, info) => {
+      subscription.register(`TEAM_UPDATED/${args.eventId}-${args.roundId}`);
     },
     resolve: async (query, root, args, ctx, info) => {
-      const data = await ctx.prisma.test.create({
-        data: {
-          name: args.name,
+      const user = await ctx.user;
+      if (!user) {
+        throw new Error("Not Authenticated");
+      }
+      if (user.role !== "JUDGE") {
+        throw new Error("Not Authorized");
+      }
+      const isJudgeOfRound = await ctx.prisma.round.findMany({
+        where: {
+          eventId: args.eventId,
+          Judges: {
+            some: {
+              userId: user.id,
+            },
+          },
         },
       });
-      ctx.pubsub.publish("test-event", { testEvent: { name: args.name } });
-      return data;
+      if (isJudgeOfRound.length === 0) {
+        throw new Error("Not Authorized");
+      }
+      const teams = await ctx.prisma.team.findMany({
+        where: {
+          eventId: args.eventId,
+          roundNo: {
+            gte: args.roundId,
+          },
+        },
+        ...query,
+      });
+      return teams;
     },
   })
 );
