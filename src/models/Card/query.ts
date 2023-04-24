3749fd9656abfd9b645cb0a487a64739e53c41a4
AaronNazareth <4nm20cs005@nmamit.in>
2023-04-24 23:40:55 +0530

    fix:getCards query

--- src/models/Card/query.ts
+++ src/models/Card/query.ts
@@ -1,9 +1,13 @@
 import { builder } from "../../builder";
 import checkIfPublicityMember from "../../publicityMembers/checkIfPublicityMember";
+import { DayType } from "@prisma/client";
 
 builder.queryField("getCards",(t) => 
     t.prismaField({
         type: ["Card"],
+        args:{
+            day: t.arg({ type:DayType,required:true })
+        },
         errors:{
             types:[Error]
         },
@@ -13,7 +17,11 @@ builder.queryField("getCards",(t) =>
                 throw new Error("Not authenticated");
             if(!checkIfPublicityMember(user.id))
                 throw new Error("Not authorized");
-            return ctx.prisma.card.findMany({})
+            return ctx.prisma.card.findMany({
+                where:{
+                    day: args.day
+                }
+            });
         }
     })
 )
