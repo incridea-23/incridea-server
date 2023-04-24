bb636f0cf02d222b39f03a091142dda29b6f9e55
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-04-24 12:37:00 +0530

    feat: mutation getScoreSheet

--- src/models/Score/query.ts
+++ src/models/Score/query.ts
@@ -203,12 +203,6 @@ builder.queryField("getTotalScores", (t) =>
           roundNo: Number(args.roundNo),
         },
       });
-      const criteria = await ctx.prisma.criteria.findMany({
-        where: {
-          eventId: Number(args.eventId),
-          roundNo: Number(args.roundNo),
-        },
-      });
 
       const total_scores = teams.map(async (team) => {
         const scores = await ctx.prisma.scores.findMany({
@@ -238,6 +232,186 @@ builder.queryField("getTotalScores", (t) =>
         };
       });
       return Promise.all(total_scores);
+    },
+  })
+);
+
+//get score as a excel sheet for a particular event and round no with criteria per judge
+/* 
+output: 
+{
+  teamName: "team1",
+  teamId: 1,
+  teamScore: 100,
+  judges: [
+    {
+      judgeName: "judge1",
+      judgeId: 1,
+      criteria: [
+        {
+          criteriaId: 1,
+          score: 10
+        },
+        {
+          criteriaId: 2,
+          score: 10
+        },
+        ...
+    }
+  ]
+}
+
+*/
+class CriteriaClass {
+  criteriaId: number;
+  score: number;
+  constructor(criteriaId: number, score: number) {
+    this.criteriaId = criteriaId;
+    this.score = score;
+  }
+}
+
+class JudgeClass {
+  judgeName: string;
+  judgeId: number;
+  criteria: CriteriaClass[];
+  constructor(judgeName: string, judgeId: number, criteria: CriteriaClass[]) {
+    this.judgeName = judgeName;
+    this.judgeId = judgeId;
+    this.criteria = criteria;
+  }
+}
+
+class ScoreSheetClass {
+  teamName: string;
+  teamId: number;
+  teamScore: number;
+  judges: JudgeClass[];
+  constructor(
+    teamName: string,
+    teamId: number,
+    teamScore: number,
+    judges: JudgeClass[]
+  ) {
+    this.teamName = teamName;
+    this.teamId = teamId;
+    this.teamScore = teamScore;
+    this.judges = judges;
+  }
+}
+
+const Criteria = builder.objectType(CriteriaClass, {
+  name: "CriteriaJuryView",
+  fields: (t) => ({
+    criteriaId: t.exposeInt("criteriaId"),
+    score: t.exposeFloat("score"),
+  }),
+});
+
+const Judge = builder.objectType(JudgeClass, {
+  name: "JudgeJuryView",
+  fields: (t) => ({
+    judgeName: t.exposeString("judgeName"),
+    judgeId: t.exposeInt("judgeId"),
+    criteria: t.expose("criteria", {
+      type: [Criteria],
+    }),
+  }),
+});
+
+const ScoreSheet = builder.objectType(ScoreSheetClass, {
+  name: "ScoreSheetJuryView",
+  fields: (t) => ({
+    teamName: t.exposeString("teamName"),
+    teamId: t.exposeInt("teamId"),
+    teamScore: t.exposeFloat("teamScore"),
+    judges: t.expose("judges", {
+      type: [Judge],
+    }),
+  }),
+});
+
+builder.queryField("getScoreSheetJuryView", (t) =>
+  t.field({
+    type: [ScoreSheet],
+    args: {
+      eventId: t.arg({ type: "ID", required: true }),
+      roundNo: t.arg({ type: "Int", required: true }),
+    },
+    errors: {
+      types: [Error],
+    },
+    resolve: async (root, args, ctx, info) => {
+      const user = await ctx.user;
+      if (!user) {
+        throw new Error("Not authenticated");
+      }
+      if (user.role !== "JURY") {
+        throw new Error("Not authorized");
+      }
+      const teams = await ctx.prisma.team.findMany({
+        where: {
+          roundNo: Number(args.roundNo),
+          eventId: Number(args.eventId),
+        },
+      });
+      const judges = await ctx.prisma.judge.findMany({
+        where: {
+          eventId: Number(args.eventId),
+          roundNo: Number(args.roundNo),
+        },
+        include: {
+          User: true,
+        },
+      });
+      const criteria = await ctx.prisma.criteria.findMany({
+        where: {
+          eventId: Number(args.eventId),
+        },
+      });
+      const scoreSheet = teams.map(async (team) => {
+        const judgesScore = judges.map(async (judge) => {
+          const scores = await ctx.prisma.scores.findMany({
+            where: {
+              teamId: team.id,
+              judgeId: judge.userId,
+            },
+          });
+          const criteriaScore = criteria.map((criterion) => {
+            const score = scores.find(
+              (score) => score.criteriaId === criterion.id
+            );
+            return {
+              criteriaId: criterion.id,
+              score: score ? Number(score.score) : 0,
+            };
+          });
+          return {
+            judgeName: judge.User.name,
+            judgeId: judge.userId,
+            criteria: criteriaScore,
+          };
+        });
+        const scores = await ctx.prisma.scores.findMany({
+          where: {
+            teamId: team.id,
+            judgeId: {
+              in: judges.map((judge) => judge.userId),
+            },
+          },
+        });
+        const totalScore = scores.reduce(
+          (acc, score) => acc + (score.score ? Number(score.score) : 0),
+          0
+        );
+        return {
+          teamName: team.name,
+          teamId: team.id,
+          teamScore: totalScore,
+          judges: await Promise.all(judgesScore),
+        };
+      });
+      return Promise.all(scoreSheet);
     },
   })
 );
