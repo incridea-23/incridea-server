cded8eeb2b80fce498141154a8bde63fc96be34f
SrivatsaRUpadhya <srivatsarupadhya@gmail.com>
2024-01-25 21:04:01 +0530

    Fix Id uploads

--- src/cloudinary/easterConfig.ts
+++ src/cloudinary/easterConfig.ts
@@ -3,10 +3,14 @@ import multer from 'multer';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
 cloudinary.config({
-    cloud_name: process.env.EASTER_CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.EASTER_CLOUDINARY_API_KEY,
-    api_secret: process.env.EASTER_CLOUDINARY_API_SECRET,
-    secure: true
+  cloud_name: "dc7z01tdd",
+  api_key: "724493754643247",
+  api_secret: "cnyL5_Qx8b69unJv63vXhrPikPU",
+  secure: true,
+    //cloud_name: process.env.EASTER_CLOUDINARY_CLOUD_NAME,
+    //api_key: process.env.EASTER_CLOUDINARY_API_KEY,
+    //api_secret: process.env.EASTER_CLOUDINARY_API_SECRET,
+    //secure: true
 });
 
 const params = {
--- src/cloudinary/idCardUploads.ts
+++ src/cloudinary/idCardUploads.ts
@@ -3,13 +3,19 @@ import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.EASTER_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.EASTER_CLOUDINARY_API_KEY,
-  api_secret: process.env.EASTER_CLOUDINARY_API_SECRET,
+  
+  cloud_name: "dc7z01tdd",
+  api_key: "724493754643247",
+  api_secret: "cnyL5_Qx8b69unJv63vXhrPikPU",
   secure: true,
+ // cloud_name: process.env.EASTER_CLOUDINARY_CLOUD_NAME,
+ // api_key: process.env.EASTER_CLOUDINARY_API_KEY,
+ // api_secret: process.env.EASTER_CLOUDINARY_API_SECRET,
+ // secure: true,
 });
 
-console.log(process.env.EASTER_CLOUDINARY_CLOUD_NAME,)
+console.log(process.env.CLOUDINARY_CLOUD_NAME,)
+console.log("Running upload")
 const params = {
   folder: "IDs",
   allowed_formats: ["jpeg", "jpg", "png"],
