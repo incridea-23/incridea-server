3338a082c5b4fc8b1e7de6856b3d95978cd1db7d
SrivatsaRUpadhya <srivatsarupadhya@gmail.com>
2024-01-28 12:01:29 +0530

    fix: patches

--- src/models/Hotel/mutation.ts
+++ src/models/Hotel/mutation.ts
@@ -18,8 +18,8 @@ builder.mutationField("createHotel", (t) =>
         throw new Error("Not authenticated");
       }
 
-      const isAllowed = checkIfAccommodationMember(user.id)
-      if(!isAllowed) throw new Error("Not allowed to perform this action")
+      const isAllowed = checkIfAccommodationMember(user.id);
+      if (!isAllowed) throw new Error("Not allowed to perform this action");
 
       //check if hotel already exists
       const isHotel = await ctx.prisma.hotel.findMany({
@@ -42,9 +42,53 @@ builder.mutationField("createHotel", (t) =>
         });
         return data;
       } catch (err) {
-		  console.log(err)
+        console.log(err);
         throw new Error("Something went wrong");
       }
     },
-  }),
+  })
+);
+
+builder.mutationField("deleteHotel", (t) =>
+  t.prismaField({
+    type: "Hotel",
+    args: {
+      hotelId: t.arg({ type: "String", required: true }),
+    },
+    errors: {
+      types: [Error],
+    },
+    resolve: async (query, root, args, ctx, info) => {
+      const user = await ctx.user;
+      if (!user) {
+        throw new Error("Not authenticated");
+      }
+
+      const isAllowed = checkIfAccommodationMember(user.id);
+      if (!isAllowed) throw new Error("Not allowed to perform this action");
+
+      //check if hotel already exists
+      const isHotel = await ctx.prisma.hotel.findUnique({
+        where: {
+          name: args.hotelId,
+        },
+      });
+      if (!isHotel) {
+        throw new Error("Hotel does not exist");
+      }
+
+      //create hotel
+      try {
+        const data = await ctx.prisma.hotel.delete({
+          where: {
+            id: Number(args.hotelId),
+          },
+        });
+        return data;
+      } catch (err) {
+        console.log(err);
+        throw new Error("Something went wrong");
+      }
+    },
+  })
 );
