ab20af89635a1538944849cf37658ee407703a19
SrivatsaRUpadhya <srivatsarupadhya@gmail.com>
2024-01-28 11:30:24 +0530

    fix: incorrect merge

---
+++ src/models/Options/index.ts
@@ -0,0 +1,19 @@
+import { builder } from "../../builder";
+
+
+
+builder.prismaObject("Options",{
+    fields: (t)=>({
+        id: t.exposeID("id"),
+        questionId: t.exposeID("questionId"),
+        question: t.relation("Question"),
+        value:t.expose("value",{
+            type:"String",
+            nullable:false
+        }),
+        isAnswer: t.exposeBoolean("isAnswer")
+
+    })
+})
+
+
