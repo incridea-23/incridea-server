0436814e30fd452301554d910d9d4a96033cfcb1
SrivatsaRUpadhya <srivatsarupadhya@gmail.com>
2024-01-28 13:10:51 +0530

    fix: login issue

--- src/models/Hotel/mutation.ts
+++ src/models/Hotel/mutation.ts
@@ -1,7 +1,6 @@
 import checkIfAccommodationMember from "../../accommodationMembers/checkIfAccommodationMembers";
 import { builder } from "../../builder";
 
-
 //mutation to create hotel
 builder.mutationField("createHotel", (t) =>
   t.prismaField({
@@ -97,33 +96,3 @@ builder.mutationField("deleteHotel", (t) =>
 );
 
 //mutation to delete Hotels
-builder.mutationField("deleteHotel",(t)=>
-  t.prismaField({
-    type:"Hotel",
-    args:{
-      id:t.arg({type:"String",required:true})
-    },
-    errors:{
-      types:[Error]
-    },
-    resolve:async(query,root,args,ctx,info)=>{
-      const user = await ctx.user;
-      if (!user) {
-        throw new Error("Not authenticated");
-      }
-      const isAllowed = checkIfAccommodationMember(user.id)
-      if(!isAllowed) throw new Error("Not allowed to perform this action")
-      //delete hotel
-      try{
-        const data = await ctx.prisma.hotel.delete({
-          where:{
-            id:Number(args.id)
-          }
-        })
-        return data
-      }catch(err){
-        throw new Error("Something went wrong")
-      }
-    }
-  })
-)
