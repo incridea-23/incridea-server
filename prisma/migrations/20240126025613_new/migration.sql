0ae6dab3fbbac0866d123999f7764f1766dc1f00
Ani <anikaranth47@gmail.com>
2024-01-28 18:23:06 +0530

    getquizsubmissions query

--- prisma/migrations/20240126025613_new/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `points` to the `Question` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `question` ADD COLUMN `negativePoints` INTEGER NOT NULL DEFAULT 0,
-    ADD COLUMN `points` INTEGER NOT NULL;
