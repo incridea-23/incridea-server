611e54b4e2f3599c5d8d28f02e55ef31d7d59a4d
swasthik <swasthikshetty10902@gmail.com>
2024-02-23 10:00:14 +0530

    fix pronites

--- src/models/ProniteRegistration/mutation.ts
+++ src/models/ProniteRegistration/mutation.ts
@@ -18,11 +18,15 @@ builder.mutationField("registerPronite", (t) =>
       if (!user) {
         throw new Error("No such user exists");
       }
-      if (user.id != 5181)
-        throw new Error("You are not allowed to register for pronite");
+      if (user.id == 466) {
+        throw new Error("Does not deserve to register for pronite");
+      }
 
       const authUser = await ctx.user;
+
       if (!authUser) throw new Error("Not authenticated");
+      if (authUser.id != 5181)
+        throw new Error("permission denied, wrong account");
       if (!["PARTICIPANT", "ORGANIZER", "BRANCH_REP"].includes(user.role)) {
         throw new Error("User did not register for the fest");
       }
